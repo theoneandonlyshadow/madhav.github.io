5078fa7d20f92b2fb012198d823ce7fdf9bdbd1e
Madhav Nair <119957011+theoneandonlyshadow@users.noreply.github.com>
2026-04-01 12:56:36 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -114,17 +114,13 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: darkModeScript }}
         />
         <Script src={`data:text/javascript;base64,${btoa(darkModeScript)}`} />
-        <Script
-          id="microsoft-clarity"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "${process.env.MICROSOFT_CLARITY_ID || ""}");`,
-          }}
-        />
+        <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "w3qbj86lhe");
+</script>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
