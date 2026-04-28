235c20f33dd8e97bb81a0b72dee52bcdc1db01e7
Madhav Nair <119957011+theoneandonlyshadow@users.noreply.github.com>
2026-04-28 15:41:39 +0530

    Add The Corvus Project to experiences description
    Updated project description to include The Corvus Project.

--- src/features/profile/data/experiences.ts
+++ src/features/profile/data/experiences.ts
@@ -15,7 +15,7 @@ export const EXPERIENCES: Experience[] = [
         },
         employmentType: "Full-time",
         icon: "code",
-        description: `- Develop [Aimver](https://aimver.com), [JustEase AI](https://dharmabot.ai/) and [Scaleberg](https://scaleberg.com).
+        description: `- Develop [Aimver](https://aimver.com), [JustEase AI](https://dharmabot.ai/), [Scaleberg](https://scaleberg.com) and The Corvus Project.
 - Develop and maintain core features to enhance functionality and user experience.
 - Ensure UI/UX consistency and adherence to standards.
 - Implement robust frontend solutions for web and mobile platforms.
