b53591e0ceed79a80cb405f6a2e4513c673fe65b
Madhav Nair <119957011+theoneandonlyshadow@users.noreply.github.com>
2026-08-08 21:16:26 +0530

    Remove isExpanded from project data
    Removed isExpanded property from project data.

--- src/features/profile/data/projects.ts
+++ src/features/profile/data/projects.ts
@@ -95,7 +95,6 @@ export const PROJECTS: Project[] = [
     description: `Cutting edge UI made with Launch UI, ShadCN and Aceternity UI. Made it for a friend. Development kept on hold due to inadequate drone data.
 - Showcases military grade drones.`,
     logo: "https://ik.imagekit.io/rwjhjllft/usasianaeroservices?updatedAt=1762454558266",
-    isExpanded: false,
   },
   {
     id: "xpr",
