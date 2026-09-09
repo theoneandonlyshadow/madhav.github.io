01641a9b535bf79241007404e613d55cc3d92f5c
Madhav Nair <119957011+theoneandonlyshadow@users.noreply.github.com>
2026-09-09 16:56:43 +0530

    Update social links with new icons and Instagram
    Updated social links icons and added Instagram link.

--- src/features/profile/data/social-links.ts
+++ src/features/profile/data/social-links.ts
@@ -2,13 +2,19 @@ import type { SocialLink } from "../types/social-links";
 
 export const SOCIAL_LINKS: SocialLink[] = [
   {
-    icon: "https://ik.imagekit.io/rwjhjllft/linkedin?updatedAt=1762444956160",
+    icon: "https://ik.imagekit.io/rwjhjllft/linkedin",
     title: "LinkedIn",
     description: "theoneandonlyshadow",
     href: "https://linkedin.com/in/theoneandonlyshadow",
   },
   {
-    icon: "https://ik.imagekit.io/rwjhjllft/github?updatedAt=1762444992661",
+    icon: "https://ik.imagekit.io/rwjhjllft/instagram",
+    title: "Instagram",
+    description: "madhvnair",
+    href: "https://www.instagram.com/madhvnair/",
+  },
+  {
+    icon: "https://ik.imagekit.io/rwjhjllft/github",
     title: "GitHub",
     description: "theoneandonlyshadow",
     href: "https://github.com/theoneandonlyshadow",
